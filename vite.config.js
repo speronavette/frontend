7f64e57df003895544e545b45b87b98c3e446879
speronavette <spero.navette@gmail.com>
2025-06-18 05:02:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -7,6 +8,13 @@ export default defineConfig({
       jsxRuntime: 'classic' // Performance optimisée - évite auto-imports React
     })
   ],
+  
+  // Configuration des alias de chemins
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   
   server: {
     port: 5173,
